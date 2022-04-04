32c60eb09fb314dfefd8eba6d1867db7ce311c60
Salamante <coskunikikardes@gmail.com>
2022-04-05 01:36:00 +0300

    small testing update

--- game/src/components/keyboard.vue
+++ game/src/components/keyboard.vue
@@ -1,28 +1,58 @@
+<script setup lang="ts">
+import { reactive, ref, onMounted, defineEmits } from 'vue';
+	const keys_first_row = ref([
+		"q", "w", "e", "r", "t", "y", "u", "ı", "o", "p", "ğ", "ü"
+	])
+	const keys_second_row = ref([
+		"a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "i"
+	])
+	const keys_third_row = ref([
+		"z", "x", "c", "v", "b", "n", "m", "ö", "ç"
+	])
+	const keys_all: any = ref([keys_first_row.value, keys_second_row.value, keys_third_row.value].flat())
+
+	const emit = defineEmits(["test"])
+	function firstEmit() {
+		emit('test', 'deneme')
+	}
+	onMounted(() => {
+		setTimeout(() => {
+			firstEmit()
+		}, 1000);
+	})
+
+	defineExpose({
+		keys_all
+	})
+</script>
 <template>
 	<div class="flex flex-col mt-10 font-bold w-full">
-		<div style="gap: 10px" class="flex flex-row justify-center content-center w-full">
-			<div v-for="(key, i) in keys_first_row" class="h-20 w-20 min-h-5 max-h-40 min-w-5 max-w-40 bg-dark-500 text-light-50 rounded-md flex items-center justify-center">
+		<div style="gap: 10px" class="flex flex-row justify-center content-center w-full cursor-pointer">
+			<div
+				v-for="(key, i) in keys_first_row"
+				class="h-20 w-20 min-h-5 max-h-40 min-w-5 max-w-40 bg-dark-500 text-light-50 rounded-md flex items-center justify-center key"
+			>
 				<span class="p-2">{{ key.toUpperCase() }}</span>
 			</div>
 		</div>
 		<div style="gap: 10px" class="flex flex-row justify-center content-center w-full mt-5">
-			<div v-for="(key, i) in keys_second_row" class="h-20 w-20 min-h-5 max-h-40 min-w-5 max-w-40 bg-dark-500 text-light-50 rounded-md flex items-center justify-center">
+			<div
+				v-for="(key, i) in keys_second_row"
+				class="h-20 w-20 min-h-5 max-h-40 min-w-5 max-w-40 bg-dark-500 text-light-50 rounded-md flex items-center justify-center"
+			>
+				<span class="p-2">{{ key != 'i' ? key.toUpperCase() : key }}</span>
+			</div>
+		</div>
+		<div style="gap: 10px" class="flex flex-row justify-center content-center w-full mt-5">
+			<div
+				v-for="(key, i) in keys_third_row"
+				class="h-20 w-20 min-h-5 max-h-40 min-w-5 max-w-40 bg-dark-500 text-light-50 rounded-md flex items-center justify-center"
+			>
 				<span class="p-2">{{ key != 'i' ? key.toUpperCase() : key }}</span>
 			</div>
 		</div>
 	</div>
 </template>
-
-<script setup lang="ts">
-import { reactive } from 'vue';
-
-const keys_first_row = reactive([
-	"q", "w", "e", "r", "t", "y", "u", "ı", "o", "p", "ğ", "ü"
-])
-const keys_second_row = reactive([
-	"a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "i"
-])
-</script>
 
 <style scoped>
 </style>
--- game/src/pages/game.vue
+++ game/src/pages/game.vue
@@ -1,17 +1,30 @@
+<script setup lang="ts">
+	import Navbar from "@/components/navbar.vue"
+	import Grid from "@/components/grid.vue"
+	import Keyboard from "@/components/keyboard.vue"
+	import { ref, onMounted } from "vue";
+
+	const kb = ref()
+	function log(payload: any) {
+		console.log(payload)
+	}
+
+	onMounted(() => {
+		document.getElementsByTagName('html')[0].addEventListener('keydown', (e) => {
+			if(kb.value.keys_all.includes(e.key)) {
+				console.log(e.key)
+			}
+		})
+	})
+</script>
 <template>
 	<div class="w-full">
 		<Navbar />
 		<div class="divider"></div>
 		<Grid />
-		<Keyboard />
+		<Keyboard ref="kb" @test="log"/>
 	</div>
 </template>
-
-<script setup lang="ts">
-import Navbar from "@/components/navbar.vue"
-import Grid from "@/components/grid.vue"
-import Keyboard from "@/components/keyboard.vue"
-</script>
 
 <style scoped>
 .divider {
